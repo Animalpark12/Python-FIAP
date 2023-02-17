6189dcb2812f303794e600ccbc9571edb98dd06d
Animalpark <gabrielplinboy@gmail.com>
2023-02-17 18:09:33 -0300

    Lista dentro de lista

--- pythonProject1/Capitulo 3 Listas/Multiplas Listas.py
+++ pythonProject1/Capitulo 3 Listas/Multiplas Listas.py
@@ -14,6 +14,35 @@ while resposta == "S":
 for indice in range(0, len(equipamentos)):
     print("Equipamento.......: ", (indice + 1))
     print("Nome..............: ", equipamentos[indice])
-    print("Valor.............: ", str(valor[indice]))
+    print("Valor.............: ", valor[indice])
+    print("Número do Serial..: ", numeroSerial[indice])
+    print("Departamento......: ", departamento[indice])
+
+busca = input("Digite o nome do equipamento que deseja buscar: ")
+for indice in range(0, len(equipamentos)):
+    if busca == equipamentos[indice]:
+        print("Valor.............: ", valor[indice])
+        print("Número do Serial..: ", numeroSerial[indice])
+        print("Departamento......: ", departamento[indice])
+
+depreciacao = input("Digite o nome do equipamento que será depreciado: ")
+for indice in range(0, len(equipamentos)):
+    if depreciacao == equipamentos[indice]:
+        print("Valor antigo: ", valor[indice])
+        valor[indice] *= 0.9
+        print("Novo Valor: ", valor[indice])
+
+danificado = int(input("Digite o serial do equipamento que será descartado: "))
+for indice in range(0, len(equipamentos)):
+    if danificado == numeroSerial[indice]:
+        del equipamentos[indice]
+        del valor[indice]
+        del numeroSerial[indice]
+        del departamento[indice]
+        break
+for indice in range(0, len(equipamentos)):
+    print("Equipamento.......: ", (indice + 1))
+    print("Nome..............: ", equipamentos[indice])
+    print("Valor.............: ", valor[indice])
     print("Número do Serial..: ", numeroSerial[indice])
     print("Departamento......: ", departamento[indice])
