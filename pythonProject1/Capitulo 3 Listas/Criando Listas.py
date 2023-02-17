e23c6977fb2b28f8d8d736bc1b0a61a803a6305b
Animalpark <gabrielplinboy@gmail.com>
2023-02-17 13:09:09 -0300

    List files

---
+++ pythonProject1/Capitulo 3 Listas/Criando Listas.py
@@ -0,0 +1,8 @@
+#Lista preenchida de forma estática
+listaEstatica = ["xpto", True]
+
+#Lista preenchida de forma dinâmica
+listaDinamica = [input("Digite o usuário: "), bool(int(input("Está logado?: ")))]
+
+#Lista Vazia
+listaVazia = []
