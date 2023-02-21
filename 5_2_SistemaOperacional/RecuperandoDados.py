f363fb14cace9e15ba0a196280eb113f1984ecd6
Animalpark <gabrielplinboy@gmail.com>
2023-02-21 12:09:24 -0300

    Sistema operacional

---
+++ 5_2_SistemaOperacional/RecuperandoDados.py
@@ -0,0 +1,10 @@
+import platform
+
+print("Destribuição do Sistema Operacional..: ", platform.platform())
+print("Nome do sistema operacional..........: ", platform.system())
+print("Versão do sistema operacional........: ", platform.release())
+print("Arquitetura..........................: ", platform.architecture())
+print("Nome da máquina......................: ", platform.node())
+print("Tipo de máquina......................: ", platform.machine())
+print("Processador..........................: ", platform.processor())
+print("Versão do Python.....................: ", platform.python_version())
